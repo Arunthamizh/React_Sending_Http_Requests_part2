464a381cd9293db693b18fce36a02287df471d57
“Arunthamizh” <arunthamizh.azhagiri@twilightitsolutions.com>
2023-12-08 19:35:23 +0530

    10th-video sending post request

--- src/App.js
+++ src/App.js
@@ -13,22 +13,24 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch('https://udemy-react-1332f-default-rtdb.firebaseio.com/movies.json');
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+
+      for(const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -39,8 +41,25 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  async function  addMovieHandler(movie) {
+    // console.log(movie);
+    try {
+    const result  = await fetch('https://udemy-react-1332f-default-rtdb.firebaseio.com/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    if (!result.ok) {
+      throw new Error('Something went wrong!');
+    }
+    const data = result.json();
+    console.log(JSON.stringify(data));
+    fetchMoviesHandler();
+  } catch (error) {
+    console.log(error);
+  }
   }
 
   let content = <p>Found no movies.</p>;
